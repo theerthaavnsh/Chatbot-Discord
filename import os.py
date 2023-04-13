0e9e5a73a69ef2300a1c985f5cf613bfafab1c36
theertha_avnsh <theerthaavnsh@gmail.com>
2023-04-13 17:52:47 +0530

    botcode

---
+++ import os.py
@@ -0,0 +1,36 @@
+import os
+import openai
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set up OpenAI API
+openai.api_key = "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"
+
+# Set up Discord bot
+TOKEN ="MTA5NjAzOTYzMTkxNDUzNzA4MA.G9dJEI.508WGlJTGkTZdbztyCee0t7p2kBoVVnvnRBUP0"
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+async def gpt3_response(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message["content"]
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if client.user.mentioned_in(message):
+        prompt = message.content.replace(f'<@!{client.user.id}>', '').strip()
+        response = await gpt3_response(prompt)
+        await message.channel.send(response)
+
+client.run(TOKEN)
